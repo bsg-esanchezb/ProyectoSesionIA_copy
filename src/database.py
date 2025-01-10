607780969b822119214f87921637f929807e2e76
Fabricio Sanchez Bullon <91036947+FabricioSaBu@users.noreply.github.com>
2025-01-10 15:39:58 -0500

    debugeo 2

--- src/database.py
+++ src/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.config import Config
 
+
+print("Use the DATABASE_URL from Config")
 # Use the DATABASE_URL from Config
 DATABASE_URL = Config.DATABASE_URL
-
+print("Create the SQLAlchemy engine")
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL, echo=False)
-
+print("Create a session factory")
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
